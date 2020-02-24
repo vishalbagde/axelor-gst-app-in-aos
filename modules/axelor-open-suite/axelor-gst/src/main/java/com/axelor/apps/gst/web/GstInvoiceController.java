f6b1ac3c2f66e60331a6b3e9bcc12eb6e2f01278
vishalbagde <vishalbagde.mmm@gmail.com>
2020-02-24 11:26:30 +0530

    minor changes in invoice line service

---
+++ modules/axelor-open-suite/axelor-gst/src/main/java/com/axelor/apps/gst/web/GstInvoiceController.java
@@ -0,0 +1,23 @@
+package com.axelor.apps.gst.web;
+
+import com.axelor.apps.account.db.Invoice;
+import com.axelor.apps.gst.service.GstInvoiceServiceImpl;
+import com.axelor.exception.AxelorException;
+import com.axelor.inject.Beans;
+import com.axelor.rpc.ActionRequest;
+import com.axelor.rpc.ActionResponse;
+
+public class GstInvoiceController {
+
+	public void verifyGstCalculateInInvoice(ActionRequest request, ActionResponse response) throws AxelorException {
+		
+		Invoice invoice = request.getContext().asType(Invoice.class);
+	
+		invoice = Beans.get(GstInvoiceServiceImpl.class).gstCalculateInInvoice(invoice);
+		response.setValue("invoiceLineList",invoice.getInvoiceLineList());
+		response.setValue("netIgst", invoice.getNetIgst());
+		response.setValue("netCgst", invoice.getNetCgst());
+		response.setValue("netSgst", invoice.getNetSgst());
+		response.setValues(invoice);				
+	}
+}
